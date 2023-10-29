6a60dcf238523e23845ffb9ba4d636c71cf070b1
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-10-29 21:27:32 +0700

    add icons

--- src/components/Icons/Icons.tsx
+++ src/components/Icons/Icons.tsx
@@ -1,3 +1,11 @@
 export { Logo, LogoMini } from "./Logo";
 export { default as Search } from "./Search";
+export { default as User } from "./User";
+export { default as HelpCircle } from "./HelpCircle";
 export { default as DotsVertical } from "./DotsVertical";
+export { default as Brush } from "./Brush";
+export { default as Settings } from "./Settings";
+export { default as MessagePlusSquare } from "./MessagePlusSquare";
+export { default as File } from "./File";
+export { default as Lock } from "./Lock";
+export { default as LogOut } from "./LogOut";
