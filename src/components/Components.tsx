e78e5270ad9566db1d6cc7e3f016714ad34cfafc
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-04 21:59:39 +0700

    add layout

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -1,3 +1,4 @@
 export { default as Navbar } from "./Navbar";
 export { default as Sidebar } from "./Sidebar";
 export { UserImage } from "./VideoComponent";
+export { default as Layout } from "./Layout";
