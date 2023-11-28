4744a30ce6685ae39f5ae08efe751790779d9bcb
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-28 20:29:19 +0700

    init footer

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -2,3 +2,4 @@ export { default as Navbar } from "./Navbar";
 export { default as Sidebar } from "./Sidebar";
 export { UserImage } from "./VideoComponent";
 export { default as Layout } from "./Layout";
+export { default as Footer } from "./Footer";
