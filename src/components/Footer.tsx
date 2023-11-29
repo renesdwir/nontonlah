c56235d2cb30fa14931a239813691fe48c409ec4
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-11-29 22:10:30 +0700

    footer style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,78 @@
+import { signIn, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import Home from "./Icons/Home";
+import ClockRewind from "./Icons/ClockRewind";
+import Folder from "./Icons/Folder";
+import UserCheck from "./Icons/UserCheck";
+import Link from "next/link";
+import { classNames } from "~/utils/helper";
+
+interface NavigationItem {
+  name: string;
+  path?: string;
+  icon: (className: string) => JSX.Element;
+  current: boolean;
+}
+
 export default function Footer() {
+  const router = useRouter();
+  const { data: sessionData } = useSession();
+  const userId = sessionData?.user.id;
+  const tabs: NavigationItem[] = [
+    {
+      name: "Home",
+      path: "/",
+      icon: (className) => <Home className={className} />,
+      current: router.pathname === "/",
+    },
+    {
+      name: "History",
+      path: userId ? "/playlist/History" : "sign-in",
+      icon: (className) => <ClockRewind className={className} />,
+      current: router.pathname === "/playlist/History",
+    },
+    {
+      name: "Library",
+      path: userId ? `/${String(userId)}/ProfilePlaylists` : "sign-in",
+      icon: (className) => <Folder className={className} />,
+      current: router.pathname === `/${String(userId)}/ProfilePlaylists`,
+    },
+    {
+      name: "Following",
+      path: userId ? `/${String(userId)}/ProfileFollowing` : "sign-in",
+      icon: (className) => <UserCheck className={className} />,
+      current: router.pathname === `/${String(userId)}/ProfileFollowing`,
+    },
+  ];
   return (
-    <>
-      <p>this is footer</p>
-    </>
+    <footer className="fixed bottom-0 z-50 w-full border border-gray-200 bg-white shadow-sm">
+      <nav className="isolate flex rounded-lg shadow" aria-label="Tabs">
+        {tabs.map((tab) => (
+          <Link
+            key={tab.name}
+            className={classNames(
+              tab.current ? "text-primary-600" : "text-gray-600",
+              "group relative min-w-0 flex-1 overflow-hidden p-4 text-center text-xs font-medium hover:bg-gray-50 focus:z-10",
+            )}
+            onClick={(e) => {
+              e.preventDefault();
+              if (tab.path === "sign-in") {
+                void signIn();
+              } else {
+                void router.push(tab.path || "/");
+              }
+            }}
+            href={"#"}
+          >
+            <div className="flex flex-col items-center">
+              {tab.current
+                ? tab.icon("h-4 w-4 shrink-0 stroke-primary-600")
+                : tab.icon("h-4 w-4 shrink-0 stroke-gray-600 ")}
+              <span>{tab.name}</span>
+            </div>
+          </Link>
+        ))}
+      </nav>
+    </footer>
   );
 }
