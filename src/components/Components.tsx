fc58d9c51e5c0a68a7910406195b5cb2fa773429
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-01 22:14:37 +0700

    add loading component

--- src/components/Components.tsx
+++ src/components/Components.tsx
@@ -3,3 +3,4 @@ export { default as Sidebar } from "./Sidebar";
 export { UserImage } from "./VideoComponent";
 export { default as Layout } from "./Layout";
 export { default as Footer } from "./Footer";
+export { default as LoadingMessage } from "./LoadingMessage";
