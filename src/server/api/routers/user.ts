9007777910928fa3c4b07e8741e08bb8871ed624
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-11 22:55:25 +0700

    init user router

---
+++ src/server/api/routers/user.ts
@@ -0,0 +1,5 @@
+import { z } from "zod";
+import { createTRPCRouter, publicProcedure } from "../trpc";
+import { EngagementType } from "@prisma/client";
+
+export const userRouter = createTRPCRouter({});
