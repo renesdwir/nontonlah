f195d96f11fbe8fef787a97da8aff40526e3a394
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-14 22:56:43 +0700

    user follow route

--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,5 +1,36 @@
 import { z } from "zod";
-import { createTRPCRouter, publicProcedure } from "../trpc";
+import { createTRPCRouter, protectedProcedure } from "../trpc";
 import { EngagementType } from "@prisma/client";
 
-export const userRouter = createTRPCRouter({});
+export const userRouter = createTRPCRouter({
+  addFollow: protectedProcedure
+    .input(z.object({ followerId: z.string(), followingId: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const existingFollow = await ctx.db.followEngagement.findMany({
+        where: {
+          followingId: input.followingId,
+          followerId: input.followerId,
+          engagementType: EngagementType.FOLLOW,
+        },
+      });
+      if (existingFollow.length > 0) {
+        const deleteFollow = await ctx.db.followEngagement.deleteMany({
+          where: {
+            followingId: input.followingId,
+            followerId: input.followerId,
+            engagementType: EngagementType.FOLLOW,
+          },
+        });
+        return deleteFollow;
+      } else {
+        const follow = await ctx.db.followEngagement.create({
+          data: {
+            followingId: input.followingId,
+            followerId: input.followerId,
+            engagementType: EngagementType.FOLLOW,
+          },
+        });
+        return follow;
+      }
+    }),
+});
