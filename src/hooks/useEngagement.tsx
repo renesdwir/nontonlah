c27818a083147ce788d2adfeb56046e0666a545e
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-16 21:46:06 +0700

    use engagement button

--- src/hooks/useEngagement.tsx
+++ src/hooks/useEngagement.tsx
@@ -1,10 +1,12 @@
+import { useState } from "react";
+
 interface useEngagementButtonProps {
   EngagementData: {
     id: string;
     likes: number;
     dislikes: number;
   };
-  Viewer: {
+  viewer: {
     hasLiked: boolean;
     hasDisliked: boolean;
   };
@@ -14,4 +16,18 @@ interface useEngagementButtonProps {
   addDislikeMutation: {
     mutate: (input: { id: string; userId: string }) => void;
   };
+}
+
+export function useEngagementButton({
+  EngagementData,
+  viewer,
+  addLikeMutation,
+  addDislikeMutation,
+}: useEngagementButtonProps) {
+  const [likeCount, setLikeCount] = useState(EngagementData.likes);
+  const [dislikeCount, setDislikeCount] = useState(EngagementData.dislikes);
+  const [userChoice, setUserChoice] = useState({
+    like: viewer.hasLiked,
+    dislike: viewer.hasDisliked,
+  });
 }
