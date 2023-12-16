191583085ea601429927735357abc20a75e5435e
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-16 21:49:18 +0700

    useengagemnt completed

--- src/hooks/useEngagement.tsx
+++ src/hooks/useEngagement.tsx
@@ -1,3 +1,4 @@
+import { EngagementType } from "@prisma/client";
 import { useState } from "react";
 
 interface useEngagementButtonProps {
@@ -30,4 +31,50 @@ export function useEngagementButton({
     like: viewer.hasLiked,
     dislike: viewer.hasDisliked,
   });
+
+  const handleEngagement =
+    (
+      engagementType: EngagementType,
+      mutateFunction: {
+        mutate: (input: { id: string; userId: string }) => void;
+      },
+    ) =>
+    (input: { id: string; userId: string }) => {
+      const { like, dislike } = userChoice;
+      const isLikeEngagement = engagementType === EngagementType.LIKE;
+
+      if (isLikeEngagement) {
+        if (like) {
+          setLikeCount(likeCount - 1);
+          setUserChoice({ like: false, dislike });
+        } else {
+          if (dislike) {
+            setDislikeCount(dislikeCount - 1);
+          }
+          setLikeCount(likeCount + 1);
+          setUserChoice({ like: true, dislike: false });
+        }
+      } else {
+        if (dislike) {
+          setDislikeCount(dislikeCount - 1);
+          setUserChoice({ like, dislike: false });
+        } else {
+          if (like) {
+            setLikeCount(likeCount - 1);
+          }
+          setDislikeCount(dislikeCount + 1);
+          setUserChoice({ like: false, dislike: true });
+        }
+      }
+
+      mutateFunction.mutate(input);
+    };
+
+  const handleLike = handleEngagement(EngagementType.LIKE, addLikeMutation);
+  const handleDislike = handleEngagement(
+    EngagementType.DISLIKE,
+    addDislikeMutation,
+  );
+
+  return { likeCount, dislikeCount, userChoice, handleLike, handleDislike };
 }
