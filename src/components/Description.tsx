1c25d7097729ed80b5778997ad52fc74dadd5ad1
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-20 21:14:02 +0700

    description video component

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -1,3 +1,54 @@
-export default function Description() {
-  return <p>Description</p>;
+import { useState } from "react";
+import { ChevronDown, ChevronUp } from "./Icons/Icons";
+
+export default function Description({
+  text,
+  length,
+  border,
+}: {
+  text: string;
+  length: number;
+  border?: boolean;
+}) {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const toggleExpand = () => {
+    setIsExpanded(!isExpanded);
+  };
+
+  if (text?.length === 0 || text === null) {
+    return null;
+  } else if (text?.length < length) {
+    return (
+      <>
+        {border ? <div className="border-b border-gray-200"></div> : ""}
+        <p className="my-3 text-left text-sm font-semibold text-gray-600">
+          {text}
+        </p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        {border ? <div className="border-b border-gray-200"></div> : ""}
+        <div className="relative w-full ">
+          <button
+            onClick={toggleExpand}
+            className="flex flex-row place-content-evenly"
+          >
+            <p
+              className={`text-left text-sm font-semibold text-gray-600 ${
+                !isExpanded ? "line-clamp-2" : ""
+              }`}
+            >
+              {text}
+            </p>
+            <span className="items-end ">
+              {isExpanded ? <ChevronUp /> : <ChevronDown />}
+            </span>
+          </button>
+        </div>
+      </>
+    );
+  }
 }
