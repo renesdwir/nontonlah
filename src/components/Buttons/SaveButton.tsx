8c89db1b0a090d493e84a26aede4a94280d92feb
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-26 21:54:22 +0700

    save button popup

--- src/components/Buttons/SaveButton.tsx
+++ src/components/Buttons/SaveButton.tsx
@@ -1,7 +1,48 @@
-export default function SaveButton() {
+import { Fragment, useState } from "react";
+import { FolderPlus } from "../Icons/Icons";
+import Button from "./Button";
+import { signIn, useSession } from "next-auth/react";
+import { Dialog, Transition } from "@headlessui/react";
+
+export default function SaveButton({ videoId }: { videoId: string }) {
+  const { data: sessionData } = useSession();
+  const [open, setOpen] = useState(false);
   return (
     <>
-      <h1>Save Button</h1>
+      <Button
+        variant="secondary-gray"
+        size="2xl"
+        onClick={sessionData ? () => setOpen(true) : () => void signIn()}
+        className="item-end flex"
+      >
+        <FolderPlus className="mr-2 h-5 w-5 shrink-0 stroke-gray-600" />
+        Save
+      </Button>
+      <Transition.Root show={open} as={Fragment}>
+        <Dialog as="div" className="relative z-10" onClose={setOpen}>
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+          </Transition.Child>
+
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-100"
+            enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            enterTo="opacity-100 translate-y-0 sm:scale-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+            leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+          ></Transition.Child>
+        </Dialog>
+      </Transition.Root>
     </>
   );
 }
