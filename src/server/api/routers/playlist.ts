02bdc7f4a2ea627b204df4fe8417cb303f69feb0
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-28 21:44:37 +0700

    get playlist data

--- src/server/api/routers/playlist.ts
+++ src/server/api/routers/playlist.ts
@@ -7,4 +7,27 @@ import {
   protectedProcedure,
 } from "~/server/api/trpc";
 
-export const playlistRouter = createTRPCRouter({});
+export const playlistRouter = createTRPCRouter({
+  getSavePlaylistData: protectedProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      const Playlists = await ctx.db.playlist.findMany({
+        where: {
+          userId: input,
+          NOT: [{ title: "Liked Videos" }, { title: "History" }],
+        },
+        include: {
+          PlaylistHasVideo: {
+            include: {
+              video: {
+                select: {
+                  id: true,
+                },
+              },
+            },
+          },
+        },
+      });
+      return Playlists;
+    }),
+});
