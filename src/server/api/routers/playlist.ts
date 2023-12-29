c25e4472eb2fed1544473ff85bf720779e67ac58
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-29 19:23:06 +0700

    add playlist route

--- src/server/api/routers/playlist.ts
+++ src/server/api/routers/playlist.ts
@@ -11,10 +11,14 @@ export const playlistRouter = createTRPCRouter({
   getSavePlaylistData: protectedProcedure
     .input(z.string())
     .query(async ({ ctx, input }) => {
-      const Playlists = await ctx.db.playlist.findMany({
+      const playlists = await ctx.db.playlist.findMany({
         where: {
           userId: input,
-          NOT: [{ title: "Liked Videos" }, { title: "History" }],
+          NOT: [
+            { title: "Liked Videos" },
+            { title: "History" },
+            { title: "Disliked Videos" },
+          ],
         },
         include: {
           PlaylistHasVideo: {
@@ -28,6 +32,24 @@ export const playlistRouter = createTRPCRouter({
           },
         },
       });
-      return Playlists;
+      return playlists;
+    }),
+  addPlaylist: protectedProcedure
+    .input(
+      z.object({
+        title: z.string(),
+        userId: z.string(),
+        description: z.string().min(5).max(50).optional(),
+      }),
+    )
+    .mutation(async ({ ctx, input }) => {
+      const playlist = await ctx.db.playlist.create({
+        data: {
+          title: input.title,
+          userId: input.userId,
+          description: input.description || "",
+        },
+      });
+      return playlist;
     }),
 });
