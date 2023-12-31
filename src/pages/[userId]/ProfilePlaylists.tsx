3380b1ec9af4b888a15ed065570d2a46b3633ea2
Renes Dwi Riwanto <renesriwanto@gmail.com>
2023-12-31 21:44:01 +0700

    profilePlaylists init

---
+++ src/pages/[userId]/ProfilePlaylists.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const ProfilePlaylists: NextPage = () => {
+  return (
+    <>
+      <h1>ProfilePlaylist</h1>
+    </>
+  );
+};
+
+export default ProfilePlaylists;
