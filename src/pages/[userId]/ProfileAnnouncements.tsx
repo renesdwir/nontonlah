c23c5ab9cf7be3722853ef302ad4fe917a48641d
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-01-06 20:19:30 +0700

    ui for announcements

--- src/pages/[userId]/ProfileAnnouncements.tsx
+++ src/pages/[userId]/ProfileAnnouncements.tsx
@@ -1,11 +1,14 @@
+import moment from "moment";
 import { NextPage } from "next";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { AnnouncementButton } from "~/components/Buttons/Buttons";
 import {
   ErrorMessage,
   Layout,
   LoadingMessage,
   ProfileHeader,
+  UserImage,
 } from "~/components/Components";
 import { api } from "~/utils/api";
 
@@ -53,9 +56,46 @@ const ProfileVideos: NextPage = () => {
           {errorTypes ? (
             <Error />
           ) : (
-            <>
-              <p>Profile Announcement</p>
-            </>
+            <ul role="list" className="-pt-8 divide-y divide-gray-200">
+              {announcements
+                ?.sort(
+                  (a, b) =>
+                    new Date(b.createdAt).getTime() -
+                    new Date(a.createdAt).getTime(),
+                )
+                .map((announcement, index) => {
+                  const user = data.user[index];
+                  if (!user) {
+                    return null;
+                  }
+                  return (
+                    <li className="pt-4" key={announcement.id}>
+                      <div className="flex gap-2">
+                        <UserImage image={user.image || ""} />
+                        <div className="flex w-full flex-col ">
+                          <div className="flex flex-col">
+                            <div className="flex flex-row items-start gap-2 text-xs">
+                              <p className="w-max text-sm font-semibold leading-6 text-gray-900">
+                                {user.name}
+                              </p>
+                              <p className="text-sm text-gray-600">
+                                {moment(announcement.createdAt).fromNow()}
+                              </p>
+                            </div>
+                            <p className="text-sm text-gray-600">
+                              {user.handle}
+                            </p>
+                          </div>
+                          <p className="my-2 text-sm text-gray-600">
+                            {announcement.message}
+                          </p>
+                          <AnnouncementButton />
+                        </div>
+                      </div>
+                    </li>
+                  );
+                })}
+            </ul>
           )}
         </>
       </Layout>
