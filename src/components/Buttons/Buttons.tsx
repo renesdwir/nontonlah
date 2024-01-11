79e80108dfb8fd6247642e324c16f5677c660a63
Renes Dwi Riwanto <renesriwanto@gmail.com>
2024-01-11 20:44:53 +0700

    add new buttons

--- src/components/Buttons/Buttons.tsx
+++ src/components/Buttons/Buttons.tsx
@@ -3,3 +3,6 @@ export { default as FollowButton } from "./FollowButton";
 export { default as LikeDislikeButton } from "./LikeDislikeButton";
 export { default as SaveButton } from "./SaveButton";
 export { default as AnnouncementButton } from "./AnnouncementButton";
+export { default as UploadButton } from "./UploadButton";
+export { default as PublishedButton } from "./PublishedButton";
+export { default as DeleteButton } from "./DeleteButton";
